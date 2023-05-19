96abd0fff6e478946cef5803f5c5763b7f6f43e7
maselko116 <kiku116m@gmail.com>
2023-05-20 00:30:02 +0200

    commit 2

---
+++ calculations.py
@@ -0,0 +1,14 @@
+num1 = int(input("podaj liczbe: "))
+num2 = int(input("podaj druga liczbe: "))
+num3 = int(input("podaj trzecia liczbe: "))
+
+def multiplication():
+    return num1*num2
+
+
+
+def adding():
+    return num1+num2+num3
+
+print(f"Wynik mnozenia to: {multiplication()}")
+print(f"Wynik dodawania to: {adding()}")
